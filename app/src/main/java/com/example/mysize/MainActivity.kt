36a21cadd5e34459077cdaae21ca692eb3e911b1
yuota <elle.226.725@gmail.com>
2020-09-10 21:23:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mysize/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mysize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.apply {
+            val editNeck = getString("NECK", "")
+            val editSleeve = getString("SLEEVE", "")
+            val editWaist = getString("WAIST", "")
+            val editInseam = getString("INSEAM", "")
+
+            neck.setText(editNeck)
+            sleeve.setText(editSleeve)
+            waist.setText(editWaist)
+            inseam.setText(editInseam)
+        }
+
+        save.setOnClickListener{ onSaveTapped()}
+    }
+
+    private fun onSaveTapped(){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.putString("NECK", neck.text.toString())
+              .putString("SLEEVE", sleeve.text.toString())
+              .putString("WAIST", waist.text.toString())
+              .putString("INSEAM", inseam.text.toString())
+              .apply()
+    }
+}
